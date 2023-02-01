0101db610562108219722607cfac9aaa43e1bf78
WuDaoJiuXiao <skg_xsn@126.com>
2023-02-01 15:38:32 +0800

    搭建微服务网关、测试成功

---
+++ xuecheng-gateway/src/main/java/com/jiuxiao/gateway/GateWayApplication.java
@@ -0,0 +1,18 @@
+package com.jiuxiao.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ * @Author: 悟道九霄 TODO:网关模块
+ * @Date: 2023年02月01日 14:24
+ * @Version: 1.0.0
+ */
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
